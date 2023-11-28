15a0f7f83be707a3d5854091b8993acc2a648b4d
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2023-11-28 13:15:08 -0300

    Add files via upload

---
+++ RECO/JavaScript.js
@@ -0,0 +1,63 @@
+const form = document.querySelector("#form");
+const nomeInput = document.querySelector("#nomesobrenome");
+const emailInput = document.querySelector("#email");
+const telefoneInput = document.querySelector("#telefone");
+const mensagemInput = document.querySelector("#mensagem");
+
+
+ 
+
+function luis(){
+  alert("Luis Henrique - Desenvolvimento Web")
+  
+}
+
+function joana(){
+  alert("Joana Falcão - Prototipação")
+  
+}
+function gustavo(){
+  alert("Gustavo Souza - Apresentação")
+  
+}
+function joyce(){
+  alert("Joyce Barbosa - Prototipação")
+  
+}
+function edson(){
+  alert("Edson Sotero - Análise")
+  
+}
+function victor(){
+  alert("Victor Souza - Apresentação")
+  
+}
+function samuel(){
+  alert("Samuel Lucas - Pesquisa")
+  
+}
+function eduarda(){
+  alert("Eduarda Veloso - Customer Experience")
+  
+}
+function isabela(){
+  alert("Isabela Araujo - Pitch")
+  
+}
+function walbert(){
+  alert("Walbert Pereira - UX/UI Designer")
+  
+}
+
+
+function BaixarAndroid(){
+  
+    
+  alert("Seu download vai começar em breve ...")
+}
+
+function BaixarApple(){
+  
+    
+  alert("Seu download vai começar em breve ...")
+}
